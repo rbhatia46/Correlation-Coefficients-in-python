32dfda5533e3671f5e52b641af548984d9e586c2
Rahul Bhatia <rbhatia46@gmail.com>
2019-05-23 11:57:23 +0530

    Create Categorical-Categorical-Unsyymetrical.py

---
+++ Categorical-Categorical-Unsyymetrical.py
@@ -0,0 +1,13 @@
+#Rule used : Theil's U
+#Range : [0,1]
+
+def theils_u(x, y):
+    s_xy = conditional_entropy(x,y)
+    x_counter = Counter(x)
+    total_occurrences = sum(x_counter.values())
+    p_x = list(map(lambda n: n/total_occurrences, x_counter.values()))
+    s_x = ss.entropy(p_x)
+    if s_x == 0:
+        return 1
+    else:
+        return (s_x - s_xy) / s_x
